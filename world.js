49fa898a7f47b6482266e0a68ee16e8b0a100439
Antonio Neil <bossneil20@gmail.com>
2022-11-14 22:42:18 -0500

    Lab Compelted Succesfully

--- world.js
+++ world.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', function(){
     let lookupButton = document.querySelector('#lookup-country');
+    let city = this.document.querySelector('#lookup');
     let country = this.document.getElementById('country');
-    let fetchName = "world.php?country=" 
+    let fetchName = "world.php?country=";
+    let lookup = "&lookup=";
+    let fetchCity = "&lookup=cities";
     
 
     lookupButton.addEventListener('click', function(e){
@@ -9,7 +12,7 @@ window.addEventListener('load', function(){
         console.log(country.value);
 
 
-        fetch(fetchName + country.value)
+        fetch(fetchName + country.value + lookup)
         .then(response => {
             if(response.ok) {
                 return response.text()
@@ -19,7 +22,7 @@ window.addEventListener('load', function(){
         })
         .then(data =>{
             
-
+            console.log(data);
             let result = document.querySelector('#result');
             result.innerHTML = data;
 
@@ -27,6 +30,32 @@ window.addEventListener('load', function(){
         })
         .catch(error => console.log('There was an error: ' + error));
     });
+
+
+    city.addEventListener('click', function(e){
+        e.preventDefault();
+        console.log(country.value);
+
+
+        fetch(fetchName + country.value + fetchCity)
+        .then(response => {
+            if(response.ok) {
+                return response.text()
+            } else {
+                return Promise.reject('Something went wrong!')
+            }
+        })
+        .then(data =>{
+            
+            console.log(data);
+            let result = document.querySelector('#result');
+            result.innerHTML = data;
+
+                
+        })
+        .catch(error => console.log('There was an error: ' + error));
+    });
+    
 
     function isEmpty(elementValue) {
         if (elementValue.length == 0) {
--- world.php
+++ world.php
@@ -4,15 +4,53 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 
+error_reporting(0);
+
 $country = $_GET['country'];
+$city = $_GET['lookup'];
+
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+if ($city == "cities"){
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities inner join countries on cities.country_code = countries.code where countries.name like '%$country%' group by cities.name");
+
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
+
+<table>
+
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>District</th>
+      <th>Population</th>
+
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach($results as $row): ?>
+      <tr>
+      <td><?= $row['name']; ?></td>
+      <td><?= $row['district']; ?></td>
+      <td><?= $row['population']; ?></td>
+      </tr>
+  <?php endforeach; ?>    
+  </tbody>
+
+</table> 
+<?php
+}else{
+  
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%'");
+
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+?>  
 
 <table>
 
@@ -36,3 +74,17 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
   </tbody>
 
 </table> 
+
+<?php } ?>
+
+
+
+
+
+
+
+
+
+
+
+
